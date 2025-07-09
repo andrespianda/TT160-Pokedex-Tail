1eff403ba072594f5d867f0eeb8fd978b729ba2e
Apianda <andres.pianda00@usc.edu.co>
2025-07-08 20:50:26 -0500

    segundo Commit

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -2,13 +2,16 @@ export default function PokeCard({ pokemon }) {
   return (
     <>
       <div className="max-w-sm rounded-4xl border border-red-900 overflow-hidden shadow-lg gap-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ml-2 mt-2">
+          {pokemon.id}
+        </span>
         <img
           className="w-full"
-          src={pokemon.pokemon.sprites.front_default}
+          src={pokemon.sprites.front_default}
           alt="Sunset in the mountains"
         />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{pokemon.pokemon.name}</div>
+          <div className="font-bold text-xl mb-2">{pokemon.name}</div>
           <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -17,7 +20,9 @@ export default function PokeCard({ pokemon }) {
         </div>
         <div className="px-6 pt-4 pb-2">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            {pokemon.pokemontipo.natural_gift_type.name}
+            {pokemon.types.map((tipo) => {
+              return `${tipo.type.name} /`;
+            })}
           </span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #travel
--- src/services/pokeapi.js
+++ src/services/pokeapi.js
@@ -4,13 +4,11 @@ export async function getPokemon(offset = 50) {
   for (let i = 1; i <= offset; i++) {
     try {
       const response = await fetch(`${API_URL}/pokemon/${i}`);
-      const resTipo = await fetch(`${API_URL}/berry/${i}`);
       const pokemon = await response.json();
-      const pokemontipo = await resTipo.json();
-      console.log(pokemontipo.natural_gift_type.name);
-      data.push({ pokemon, pokemontipo });
+      console.log(pokemon.types);
+      data.push(pokemon);
 
-      console.log(data);
+      // console.log(data);
     } catch (error) {
       console.error(error);
       return false;
